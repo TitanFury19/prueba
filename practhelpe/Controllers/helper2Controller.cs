834b228c4e667d69de0ec6082f4d46099173b8cd
JOSE ZAPATA <josegabriel.jgcz19@hotmail.com>
2019-03-30 00:52:00 -0400

    version1

---
+++ practhelpe/Controllers/helper2Controller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using practhelpe.Models;
+
+namespace practhelpe.Controllers
+{
+    public class helper2Controller : Controller
+    {
+        // GET: helper2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult formulario()
+        {
+            return View();
+        }
+        public ActionResult datos()
+        {
+            if (ModelState.IsValid) 
+            {
+                Class1 c = new Class1();
+
+                c.cedula = Request.Form["cedula"].ToString();
+                c.nombre = Request.Form["nombre"].ToString();
+                c.apellido = Request.Form["apellido"].ToString();
+                c.edad = Convert.ToInt32(Request.Form["edad"].ToString());
+                c.telefono = Request.Form["telefono"].ToString();
+                c.correo = Request.Form["correo"].ToString();
+                c.genero = Request.Form["genero"].ToString();
+                c.estadoc = Request.Form["estadoc"].ToString();
+                c.hobbys = (Request.Form["hobbys"].ToString());
+
+                               
+
+                return View(c);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ practhelpe/Models/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace practhelpe.Models
+{
+    public class Class1
+    {
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string cedula{ get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string nombre { get; set; }
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string apellido { get; set; }
+
+        [Range(15,70,ErrorMessage ="mayor de 15 y menor de 70")]
+        public int edad { get; set; }
+
+        public string telefono { get; set; }
+
+        [EmailAddress(ErrorMessage ="formato de correo invalido")]
+        public string  correo { get; set; }
+
+        public string  genero { get; set; }
+
+       
+        public string estadoc { get; set; }
+        public String hobbys { get; set; }
+    }
+ 
+}
